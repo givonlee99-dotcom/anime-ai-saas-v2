6268f2263e3410085c42e0652e7c587572d1e6d2
givonlee99-dotcom <givonlee99@gmail.com>
2026-05-14 16:59:20 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -305,9 +305,7 @@ export default function Home() {
             <div className="flex items-center justify-between mb-5">
 
               <h2 className="text-4xl md:text-5xl font-black">
-                Upload
-                <br />
-                Gambar
+                Upload Gambar
               </h2>
 
               <div className="px-4 py-2 rounded-full bg-cyan-500/20 text-cyan-300 text-sm">
@@ -339,7 +337,7 @@ export default function Home() {
                   </div>
 
                   <p className="text-2xl font-bold">
-                    Upload IMG
+                    Upload Gambar
                   </p>
 
                   <p className="text-gray-500 mt-3">
